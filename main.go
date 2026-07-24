21ed84cdb4b6ea3338051730a5084f92edb5cad8
0xgrowth <sapientiakonzept@gmail.com>
2026-07-24 15:30:11 +0100

    Initialize repo

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+func menu() {
+	r := `==== Student Manager ====
+
+	1. Add Student
+	2. View Students
+	3. Exit
+
+	Select ? 
+	`
+	fmt.Println(r)
+}
+
+func main(){
+
+}
---
+++ student.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Student struct {
+    ID    int
+    Name  string
+    Age   int
+    Score float64
+}
+func addStudent(students []Student) []Student {
+	a := Student{
+		ID: 0,
+		Name: "John",
+		Age: 19,
+		Score: 50.7,
+	}
+	b := Student{
+		ID: 1,
+		Name: "Susan",
+		Age: 18,
+		Score: 78.7,
+	}
+	c := Student{
+		ID: 2,
+		Name: "Jake",
+		Age: 19,
+		Score: 89.5,
+	}
+	d := Student{
+		ID: 3,
+		Name: "Jane",
+		Age: 20,
+		Score: 50.0,
+	}
+}
+
+func displayStudents(students []Student) {
+
+}
+
+func Student() {
+	students := []Student{}
+	for {
+		menu()
+	
+		// read user's choice
+	
+		switch choice {
+		case 1:
+			// add
+			time.Sleep(time.Second)
+			students = addStudent(students)
+		case 2:
+			// view
+			time.Sleep(time.Second)
+			displayStudents(student)
+		case 6:
+			return
+		}
+	}
+
+}
